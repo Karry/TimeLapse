90deeb660839eafbf5344bd0e85a3c42e6e47a88
Lukáš Karas <lukas.karas@centrum.cz>
2020-12-18 13:04:00 +0100

    CMake: remove lib prefix from v4l and vidstab variables

--- CMakeMod/FindV4L.cmake
+++ CMakeMod/FindV4L.cmake
@@ -1,31 +1,31 @@
 # - Try to find LibV4L
 # Once done this will define
-#  LIBV4L_FOUND - System has LibV4L
-#  LIBV4L_INCLUDE_DIRS - The LibV4L include directories
-#  LIBV4LCONVERT_LIBRARY
-#  LIBV4L_LIBRARIES - The libraries needed to use LibV4L
-#  LIBV4L_DEFINITIONS - Compiler switches required for using LibV4L
+#  V4L_FOUND - System has LibV4L
+#  V4L_INCLUDE_DIRS - The LibV4L include directories
+#  V4LCONVERT_LIBRARY
+#  V4L_LIBRARIES - The libraries needed to use LibV4L
+#  V4L_DEFINITIONS - Compiler switches required for using LibV4L
 
 find_package(PkgConfig)
-pkg_check_modules(PC_LIBV4L QUIET libv4l)
-set(LIBV4L_DEFINITIONS ${PC_LIBV4L_CFLAGS_OTHER})
+pkg_check_modules(PC_V4L QUIET libv4l)
+set(V4L_DEFINITIONS ${PC_V4L_CFLAGS_OTHER})
 
-find_path(LIBV4L_INCLUDE_DIR libv4l2.h libv4lconvert.h
-          HINTS ${PC_LIBV4L_INCLUDEDIR} ${PC_LIBV4L_INCLUDE_DIRS}
+find_path(V4L_INCLUDE_DIR libv4l2.h libv4lconvert.h
+          HINTS ${PC_V4L_INCLUDEDIR} ${PC_V4L_INCLUDE_DIRS}
           PATH_SUFFIXES libv4l )
 
-find_library(LIBV4L_LIBRARY NAMES v4l2 
-             HINTS ${PC_LIBV4L_LIBDIR} ${PC_LIBV4L_LIBRARY_DIRS} )
-find_library(LIBV4LCONVERT_LIBRARY NAMES v4lconvert
-             HINTS ${PC_LIBV4L_LIBDIR} ${PC_LIBV4L_LIBRARY_DIRS} )
+find_library(V4L_LIBRARY NAMES v4l2
+             HINTS ${PC_V4L_LIBDIR} ${PC_V4L_LIBRARY_DIRS} )
+find_library(V4LCONVERT_LIBRARY NAMES v4lconvert
+             HINTS ${PC_V4L_LIBDIR} ${PC_V4L_LIBRARY_DIRS} )
 
-set(LIBV4L_LIBRARIES ${LIBV4L_LIBRARY} ${LIBV4LCONVERT_LIBRARY})
-set(LIBV4L_INCLUDE_DIRS ${LIBV4L_INCLUDE_DIR} )
+set(V4L_LIBRARIES ${V4L_LIBRARY} ${V4LCONVERT_LIBRARY})
+set(V4L_INCLUDE_DIRS ${V4L_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LIBV4L_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set V4L_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(LibV4L  DEFAULT_MSG
-                                  LIBV4L_LIBRARY LIBV4LCONVERT_LIBRARY LIBV4L_INCLUDE_DIR)
+find_package_handle_standard_args(V4L  DEFAULT_MSG
+                                  V4L_LIBRARY V4LCONVERT_LIBRARY V4L_INCLUDE_DIR)
 
-mark_as_advanced(LIBV4L_INCLUDE_DIR LIBV4L_LIBRARY )
+mark_as_advanced(V4L_INCLUDE_DIR V4L_LIBRARY )
--- CMakeMod/FindVidStab.cmake
+++ CMakeMod/FindVidStab.cmake
@@ -1,28 +1,28 @@
 # - Try to find LibVidStab
 # Once done this will define
-#  LIBVIDSTAB_FOUND - System has LibVidStab
-#  LIBVIDSTAB_INCLUDE_DIRS - The LibVidStab include directories
-#  LIBVIDSTAB_LIBRARIES - The libraries needed to use LibVidStab
-#  LIBVIDSTAB_DEFINITIONS - Compiler switches required for using LibVidStab
+#  VIDSTAB_FOUND - System has LibVidStab
+#  VIDSTAB_INCLUDE_DIRS - The LibVidStab include directories
+#  VIDSTAB_LIBRARIES - The libraries needed to use LibVidStab
+#  VIDSTAB_DEFINITIONS - Compiler switches required for using LibVidStab
 
 find_package(PkgConfig)
-pkg_check_modules(PC_LIBVIDSTAB QUIET libvidstab)
-set(LIBVIDSTAB_DEFINITIONS ${PC_LIBVIDSTAB_CFLAGS_OTHER})
+pkg_check_modules(PC_VIDSTAB QUIET libvidstab)
+set(VIDSTAB_DEFINITIONS ${PC_VIDSTAB_CFLAGS_OTHER})
 
-find_path(LIBVIDSTAB_INCLUDE_DIR vid.stab/libvidstab.h
-          HINTS ${PC_LIBVIDSTAB_INCLUDEDIR} ${PC_LIBVIDSTAB_INCLUDE_DIRS}
+find_path(VIDSTAB_INCLUDE_DIR vid.stab/libvidstab.h
+          HINTS ${PC_VIDSTAB_INCLUDEDIR} ${PC_VIDSTAB_INCLUDE_DIRS}
           PATH_SUFFIXES libvidstab )
 
-find_library(LIBVIDSTAB_LIBRARY NAMES vidstab libvidstab
-             HINTS ${PC_LIBVIDSTAB_LIBDIR} ${PC_LIBVIDSTAB_LIBRARY_DIRS} )
+find_library(VIDSTAB_LIBRARY NAMES vidstab libvidstab
+             HINTS ${PC_VIDSTAB_LIBDIR} ${PC_VIDSTAB_LIBRARY_DIRS} )
 
-set(LIBVIDSTAB_LIBRARIES ${LIBVIDSTAB_LIBRARY} )
-set(LIBVIDSTAB_INCLUDE_DIRS ${LIBVIDSTAB_INCLUDE_DIR} )
+set(VIDSTAB_LIBRARIES ${VIDSTAB_LIBRARY} )
+set(VIDSTAB_INCLUDE_DIRS ${VIDSTAB_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LIBVIDSTAB_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set VIDSTAB_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(LibVidStab  DEFAULT_MSG
-                                  LIBVIDSTAB_LIBRARY LIBVIDSTAB_INCLUDE_DIR)
+find_package_handle_standard_args(VidStab  DEFAULT_MSG
+                                  VIDSTAB_LIBRARY VIDSTAB_INCLUDE_DIR)
 
-mark_as_advanced(LIBVIDSTAB_INCLUDE_DIR LIBVIDSTAB_LIBRARY )
+mark_as_advanced(VIDSTAB_INCLUDE_DIR VIDSTAB_LIBRARY )

c8fee181c71c9ce0966656297cabc2890a9e0f41
Lukáš Karas <lukas.karas@centrum.cz>
2022-08-02 10:48:54 +0200

    improve v4l libraries detection

--- CMakeMod/FindV4L.cmake
+++ CMakeMod/FindV4L.cmake
@@ -7,17 +7,19 @@
 #  V4L_DEFINITIONS - Compiler switches required for using LibV4L
 
 find_package(PkgConfig)
-pkg_check_modules(PC_V4L QUIET libv4l)
+pkg_check_modules(PC_V4L QUIET libv4l2)
+pkg_check_modules(PC_V4L_CONVERT QUIET libv4l2convert)
+
 set(V4L_DEFINITIONS ${PC_V4L_CFLAGS_OTHER})
 
 find_path(V4L_INCLUDE_DIR libv4l2.h libv4lconvert.h
-          HINTS ${PC_V4L_INCLUDEDIR} ${PC_V4L_INCLUDE_DIRS}
+          HINTS ${PC_V4L_INCLUDE_DIRS} ${PC_V4L_CONVERT_INCLUDE_DIRS}
           PATH_SUFFIXES libv4l )
 
 find_library(V4L_LIBRARY NAMES v4l2
-             HINTS ${PC_V4L_LIBDIR} ${PC_V4L_LIBRARY_DIRS} )
+             HINTS ${PC_V4L_LIBRARY_DIRS} )
 find_library(V4LCONVERT_LIBRARY NAMES v4lconvert
-             HINTS ${PC_V4L_LIBDIR} ${PC_V4L_LIBRARY_DIRS} )
+             HINTS ${PC_V4L_CONVERT_LIBRARY_DIRS} )
 
 set(V4L_LIBRARIES ${V4L_LIBRARY} ${V4LCONVERT_LIBRARY})
 set(V4L_INCLUDE_DIRS ${V4L_INCLUDE_DIR} )
